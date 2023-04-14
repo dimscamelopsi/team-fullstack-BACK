0c61bfe78591ba9d6061ccf0bb8ab2400417a78f
boudi1983 <boudi.mohand@gmail.com>
2023-04-14 20:04:53 +0200

    Add media finiched

--- src/main/java/fr/aelion/streamer/services/ModuleService.java
+++ src/main/java/fr/aelion/streamer/services/ModuleService.java
@@ -1,13 +1,17 @@
 package fr.aelion.streamer.services;
+
 import fr.aelion.streamer.dto.ModuleAddDto;
 import fr.aelion.streamer.dto.ModuleDto;
 import fr.aelion.streamer.entities.Course;
+import fr.aelion.streamer.entities.Media;
 import fr.aelion.streamer.entities.Module;
+import fr.aelion.streamer.repositories.ModuleMediaRepository;
 import fr.aelion.streamer.repositories.ModuleRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,23 +20,39 @@ public class ModuleService {
     @Autowired
     private ModuleRepository repository;
     @Autowired
+    private ModuleMediaRepository moduleMediaRepository;
+    @Autowired
     private ModelMapper modelMapper;
-
     @Autowired
     private CourseServiceImpl courseService;
     public ModuleDto add(ModuleAddDto moduleAddDto) {
         Module newModule = new Module();
         newModule.setName(moduleAddDto.getName());
         newModule.setObjective(moduleAddDto.getObjective());
-        Course course= new Course();
-        if(moduleAddDto.getCourse()!=null) {
+        Course course = new Course();
+        if (moduleAddDto.getCourse() != null) {
             course.setId(moduleAddDto.getCourse().getId());
             newModule.setCourse(course);
         }
+        if (moduleAddDto.getMedia() != null) {
+            List<Media> medias = new ArrayList<>();
+            moduleAddDto.getMedia().forEach(mDto -> {
+
+                var media = modelMapper.map(mDto, Media.class);
+                medias.add(media);
+            });
+            newModule.setMedias(medias);
+
+        }
         newModule = repository.save(newModule);
+
         return modelMapper.map(newModule, ModuleDto.class);
     }
 
+    /**
+     *
+     * @return
+     */
     public List<ModuleDto> findAll() {
         var modules = repository.findAll()
                 .stream()
@@ -45,7 +65,7 @@ public class ModuleService {
                 .collect(Collectors.toList());
 
         return modules;
-    };
+    }
 
 
 }
