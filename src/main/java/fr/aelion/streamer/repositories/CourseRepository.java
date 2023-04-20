1b7a2f9adbae124eac859c3275e8347942ce857f
redg34 <98891221+redg34@users.noreply.github.com>
2023-04-20 18:25:46 +0200

    mohand taf and dany

--- src/main/java/fr/aelion/streamer/repositories/CourseRepository.java
+++ src/main/java/fr/aelion/streamer/repositories/CourseRepository.java
@@ -7,6 +7,8 @@ import fr.aelion.streamer.entities.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import java.util.List;
+import java.util.Optional;
 
 import java.util.Collection;
 import java.util.List;
@@ -17,5 +19,11 @@ public interface CourseRepository extends JpaRepository<Course, Integer> {
             nativeQuery = true)
     List<Course> findAllCourseUsersNative(@Param("personne_id") int personne_id);
     List<Course> findCoursesByStudent(Student student);
+
+    @Query(value = "SELECT * FROM public.course c WHERE c.personne_id = id", nativeQuery = true)
+    List<Course> findByPersonneId(@Param("id") int id);
+
+    @Query("SELECT s FROM Student s WHERE s.login = :login AND s.password = :password")
+    Optional<Student> findByLoginAndPassword(@Param("login") String login, @Param("password") String password);
 
 }
