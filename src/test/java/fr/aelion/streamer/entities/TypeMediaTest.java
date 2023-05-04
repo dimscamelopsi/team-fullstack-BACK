9581df974b218fb350d5147c64b4f20265986a8c
boudi1983 <boudi.mohand@gmail.com>
2023-05-04 09:58:59 +0200

    add test course, media, module, typeMedia

---
+++ src/test/java/fr/aelion/streamer/entities/TypeMediaTest.java
@@ -0,0 +1,20 @@
+package fr.aelion.streamer.entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TypeMediaTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void getMedias() {
+    }
+}
